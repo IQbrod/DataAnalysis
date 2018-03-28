5514ce3716faae2b830751dd3f098d684a63543f
MrVhek <adri73390@hotmail.fr>
2018-03-28 10:57:57 +0200

    + ajout classe Dataframe

---
+++ src/Dataframe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author michauad
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Dataframe {
+    private HashMap<Object, Integer> indexs;
+    private HashMap<Object, ArrayList<Object>> labels;
+
+    //Constructeur avec tableaux
+    public Dataframe(List<Object> indexs, List<Object> labels, List<Object>... data) {
+        //Construction des indexs
+        
+    }
+}
+
