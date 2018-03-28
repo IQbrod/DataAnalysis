bee8f7b1eef16882f6344ddffa602b12b176e3c8
IQbrod <iqbrod@outlook.fr>
2018-03-28 11:23:11 +0200

    Refactor authors

--- src/Dataframe.java
+++ src/Dataframe.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author michauad
+ * @author MrVhek, IQbrod, Skullhack
  */
 import java.util.ArrayList;
 import java.util.HashMap;
