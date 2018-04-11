8106740471a050d275ff368af4f42ba20956f14e
Skullhack <vincent.givaudan@hotmail.fr>
2018-04-11 08:52:21 +0200

    ~ modified project structure

---
+++ src/test/java/com/mogor/mogoranalysor/DataframeTest.java
@@ -0,0 +1,22 @@
+package com.mogor.mogoranalysor;
+
+import org.junit.Before;
+
+
+
+/**
+ *
+ * @author MrVhek, IQbrod, Skullhack
+ */
+
+public class DataframeTest {
+    protected Dataframe subjectTab;
+    protected Dataframe subjectFile;
+
+    @Before
+    public void setup() {
+        subjectTab = new Dataframe();
+        subjectTab = new Dataframe();
+    }
+}
+
