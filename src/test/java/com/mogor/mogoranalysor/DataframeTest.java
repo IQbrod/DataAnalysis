5036c1a7751209107cd72bc3f46d282567bc8d27
IQbrod <iqbrod@outlook.fr>
2018-04-11 11:07:05 +0200

    + Tests Constructeur CSV

--- src/test/java/com/mogor/mogoranalysor/DataframeTest.java
+++ src/test/java/com/mogor/mogoranalysor/DataframeTest.java
@@ -1,5 +1,9 @@
 package com.mogor.mogoranalysor;
 
+import com.mogor.mogoranalysor.exceptions.*;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,14 +18,32 @@ import org.junit.Test;
 public class DataframeTest {
     private Dataframe subjectFile;
 
-    @Before
-    public void setup() throws Exception {
-        subjectFile = new Dataframe("data/people.csv");
-    }
-    
     @Test
     public void testFullDisplay() {
+        try {
+            subjectFile = new Dataframe("data/people.csv");
+        } catch (Exception ex) {
+            Assert.fail("No Exception should be thrown for data/people.csv");
+        }
         Assert.assertEquals("Index\tage\tsex\t\nJohn\t30\tH\t\nMary\t29\tF\t\nAnna\t18\tF\t\n", subjectFile.toString());
     }
+    /**  TEST CONSTRUCTEUR  **/
+    /**  Constructeur CSV  **/
+    @Test (expected = FileNotFoundException.class)
+    public void testUnexistingConstructorCSV() throws Exception {
+        subjectFile = new Dataframe("myfile.csv");
+    }
+    
+    @Test (expected = ExtensionException.class)
+    public void testNoExtensionConstructorCSV() throws Exception {
+        subjectFile = new Dataframe("myfile");
+    }
+    
+    @Test (expected = ExtensionException.class)
+    public void testWrongExtensionConstructorCSV() throws Exception {
+        subjectFile = new Dataframe("myfile.java");
+    }
+    
+    /**  TEST METHODES  **/
 }
 
