9d050583ee23a127f73ab44e8e3f47051d9b155e
IQbrod <iqbrod@outlook.fr>
2018-04-13 13:05:01 +0200

    Correction useless lines CheckTyper

--- src/main/java/com/mogor/mogoranalysor/CheckTyper.java
+++ src/main/java/com/mogor/mogoranalysor/CheckTyper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mogor.mogoranalysor;
 
 import com.mogor.mogoranalysor.exceptions.TypeException;
@@ -11,12 +6,9 @@ import java.util.List;
 
 /**
  *
- * @author adrien
+ * @author MrVhek, IQbrod, Skullhack
  */
 public final class CheckTyper {
-
-    private CheckTyper() {
-    }
 
     public static void checkType(List lst) throws TypeException {
         Class<?> type = null;
