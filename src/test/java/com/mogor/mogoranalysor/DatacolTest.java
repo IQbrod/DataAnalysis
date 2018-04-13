84345f7913afdbafe362301f9fb24a8c9f5fa795
Skullhack <vincent.givaudan@hotmail.fr>
2018-04-13 13:18:57 +0200

    test Datacol

--- src/test/java/com/mogor/mogoranalysor/DatacolTest.java
+++ src/test/java/com/mogor/mogoranalysor/DatacolTest.java
@@ -5,7 +5,9 @@
  */
 package com.mogor.mogoranalysor;
 
+import com.mogor.mogoranalysor.exceptions.TypeException;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -17,10 +19,41 @@ import org.junit.Test;
 public class DatacolTest {
     private Datacol subject;
 
-    /**  TEST CONSTRUCTEUR  **/
+    /**  TEST CONSTRUCTEUR
+     * @throws java.lang.Exception **/
     @Test
-    public void testConstructor() throws Exception {
-        
+    public void testConstructorStringValid() throws Exception {
+        try {
+            subject = new Datacol("colTest", Arrays.asList("bonjour", "F"));
+        } catch (Exception ex) {
+            Assert.fail("No Exception should be thrown for a string only constructor.");
+        }
+        Assert.assertTrue("Constructor Passed", true);
+    }
+    
+    @Test
+    public void testConstructorIntValid() throws Exception {
+        try {
+            subject = new Datacol(4, Arrays.asList(6, 10));
+        } catch (Exception ex) {
+            Assert.fail("No Exception should be thrown for an integer only constructor.");
+        }
+        Assert.assertTrue("Constructor Passed", true);
+    }
+    
+    @Test
+    public void testConstructorDifferentLabelListTypeValid() throws Exception {
+        try {
+            subject = new Datacol(4.8, Arrays.asList("bonjour", "F"));
+        } catch (Exception ex) {
+            Assert.fail("No Exception should be thrown for different label/list types.");
+        }
+        Assert.assertTrue("Constructor Passed", true);
+    }
+    
+    @Test (expected = TypeException.class)
+    public void testConstructorDifferentListType() throws Exception {
+        subject = new Datacol("colTest", Arrays.asList(1, "F", 4.3));
     }
     
     /**  TEST METHODES  **/
