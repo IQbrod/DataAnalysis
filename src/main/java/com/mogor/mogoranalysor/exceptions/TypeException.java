9e541cca48178e0ecce11e826083ef173c11abb6
IQbrod <iqbrod@outlook.fr>
2018-04-13 12:59:44 +0200

    Removing useless constructor for TypeException

--- src/main/java/com/mogor/mogoranalysor/exceptions/TypeException.java
+++ src/main/java/com/mogor/mogoranalysor/exceptions/TypeException.java
@@ -5,10 +5,6 @@ package com.mogor.mogoranalysor.exceptions;
  * @author MrVhek, IQbrod, Skullhack
  */
 public class TypeException extends Exception {
-    public TypeException(String expected, String given) {
-        super("Expected Type : "+expected+": . "+given);
-    }
-    
     public TypeException(Class<?> expected, Class<?> given) {
         super("Expected Type : "+expected.toString()+": . "+given.toString());
     }
--- src/test/java/com/mogor/mogoranalysor/DataframeTest.java
+++ src/test/java/com/mogor/mogoranalysor/DataframeTest.java
@@ -5,8 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
